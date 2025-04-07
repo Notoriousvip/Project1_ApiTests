74b376c9e3d3446ede2832f092a35d87f1822e2f
eva <primerxo@mail.ru>
2025-04-07 19:23:42 +0600

    добавила тесты на POST, PUT, PATCH, DELETE из reqres.in

--- core/contracts.py
+++ core/contracts.py
@@ -21,3 +21,23 @@ LIST_RESOURCE_SCHEME = {
     },
     "required" : ["id", "name", "year", "color", "pantone_value"]
 }
+
+CREATED_USER_SCHEME = {
+    "type" : "object",
+    "properties" : {
+        "name" : {"type" : "string"},
+        "job" : {"type" : "string"},
+        "id" : {"type" : "string"}
+    },
+    "required" : ["id"]
+}
+
+UPDATED_USER_SCHEME = {
+    "type" : "object",
+    "properties" : {
+        "name" : {"type" : "string"},
+        "job" : {"type" : "string"},
+        "updatedAt" : {"type" : "string"}
+    },
+    "required" : ["name", "job", "updatedAt"]
+}
